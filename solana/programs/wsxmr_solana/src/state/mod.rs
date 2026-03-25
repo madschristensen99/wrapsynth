bf568c23566402a95541d4fa43bc8a767236cd31
Your Name <you@example.com>
2026-03-25 15:12:00 -0400

    Remove Solana implementation and update spec

--- solana/programs/wsxmr_solana/src/state/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod global_state;
-pub mod vault;
-pub mod mint_request;
-pub mod burn_request;
-
-pub use global_state::*;
-pub use vault::*;
-pub use mint_request::*;
-pub use burn_request::*;
